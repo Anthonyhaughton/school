e83cce2fc1e0edf9e6bd7e6565ea51a90192e1cb
Anthony <a@wgu.edu>
2025-02-06 20:54:39 -0500

    init commit

---
+++ promts.ps1
@@ -0,0 +1,13 @@
+# Anthony Haughton                      # Student ID:
+
+
+do {
+    # Display menu
+    Write-Host "Menu:"
+    Write-Host "1. List .log files in folder"
+    Write-Host "2. List filesin folder in alphabetical order"
+    Write-Host "3. Show current CPU and memory usage"
+    Write-Host "4. List all running processes"
+    Write-Host "5. Exit"
+
+}
